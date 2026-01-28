df0545662339e031682051dd0677102bf0e0aa86
Colson Cissel <colson.cissel@gmail.com>
2026-01-27 21:41:45 -0600

    expose exports related to async function call handling in dist/index.js

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { Database } from './database'
-export { Reactor } from './reactor'
+export { asyncCallResult, AsyncCallStatus, asyncCallStatus, Reactor } from './reactor'
 export { useResult } from './reactor_hooks'
