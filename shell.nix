af07f283367eb90b9e678cd76e39b42e2baf15f6
Colson Cissel <colson.cissel@gmail.com>
2026-09-02 10:42:21 -0500

    remove Claude Code from environment

--- shell.nix
+++ shell.nix
@@ -1,13 +1,9 @@
 let
   pkgs = import <nixpkgs> {};
-  unstable = import <nixos-unstable> {
-    config.allowUnfree = true;
-  };
 in
 
 pkgs.mkShell {
   packages = with pkgs; [
     nodejs
-    unstable.claude-code
   ];
 }
